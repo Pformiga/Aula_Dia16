dd49115c00fa0799c173340aa037b29461a92c84
Pformiga <pedroformigacaires@hotmail.com>
2024-08-16 17:54:13 -0300

    Add files via upload

---
+++ diarioalomundo/aluno.py
@@ -0,0 +1,8 @@
+class Aluno():
+    def __init__(self,ra,nome,tempoestudo,rendafamiliar):
+        self.ra = ra
+        self.nome = nome
+        self.nome = tempoestudo
+        self.nome = rendafamiliar
+
+        
